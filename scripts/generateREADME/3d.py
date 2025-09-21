6d8b90d8d994c77916f9fc707fcb94374f9dd65d
VDN <victor.d.nikolaev@gmail.com>
2025-09-21 22:16:47 +0300

    fix ./scripts/generateREADME/3d.py

--- scripts/generateREADME/3d.py
+++ scripts/generateREADME/3d.py
@@ -7,26 +7,27 @@ def main():
     parser.add_argument('directory', type=str, help='Path to the directory to scan')
     args = parser.parse_args()
 
-    # Интересующие нас расширения
+    # Extensions
     extensions = {'.m3d', '.stp', '.png'}
     groups = defaultdict(set)
 
-    # Сканируем директорию и группируем файлы
+    # Scanning directory and generate files
     for filename in os.listdir(args.directory):
         name, ext = os.path.splitext(filename)
         if ext.lower() in extensions:
             groups[name].add(ext.lower())
 
-    # Создаем markdown-файл
+    # Creating README.md
     output_path = os.path.join(args.directory, 'README.md')
     with open(output_path, 'w', encoding='utf-8') as md_file:
+        # Write README-handle
         md_file.write(f'# {args.directory.split("/")[-1]}\n\n---\n\n')
 
         for name in sorted(groups.keys()):
-            # Записываем заголовок
+            # Write handle
             md_file.write(f'# {name}\n\n')
 
-            # Вставляем изображение, если есть PNG
+            # Insert image
             if '.png' in groups[name]:
                 md_file.write(f'''<table>
 <tr valign="top">
@@ -34,7 +35,7 @@ def main():
 </tr>
 </table>\n\n''')
 
-            # Генерируем ссылки для всех форматов
+            # Generate links for all extensions
             links = []
             for ext in sorted(extensions):
                 if ext in groups[name]:
